8117103646de4cc3f3cd8ae60cf097d29ed5c89a
jihad-hossain1 <jihadkhan934@gmail.com>
2025-04-13 15:40:18 +0600

    fix added read

---
+++ README.md
@@ -0,0 +1,7 @@
+- HOW TO USE
+
+run this on terminal
+ 
+```bash 
+php -S localhost:8000
+```
--- day-1/loop.php
+++
@@ -1,9 +0,0 @@
-$fruits = ["apple", "banana", "orange"];
-
-foreach  ($fruits as $fruit){
-    echo $fruit . "<br>";
-}
-
-for ($i = 0; $i < count($fruits); $i++){
-    echo $fruits[$i]. "<br>"; 
-}
